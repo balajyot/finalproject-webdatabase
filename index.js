493eb7cbaae7b61846f073f85132264660d992a5
balajyot <147068667+balajyot@users.noreply.github.com>
2023-12-06 15:51:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,71 @@
-const http = require('http') ; 
+const { MongoClient } = require('mongodb');
 const fs = require('fs');
+const http = require('http'); 
 const path = require('path');
-http.createServer((req, res) =>{
-    console.log(req.url);
-    if(req.url ==='/'){ // home page
 
-       fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+const uri = "mongodb+srv://jyothiravali007:Ravali%4012345@cluster0.e6yfrtx.mongodb.net/?retryWrites=true&w=majority";
 
-        if (err) throw err ;
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end(content)
+const dbName = 'TopStories';
 
-       })
+// Name of the collection you want to export
+const collectionName = 'Stories';
 
+const client = new MongoClient(uri);
+
+async function fetchPlayers() {
+    
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!")
+
+        const database = client.db(dbName); 
+        const collection = database.collection(collectionName); 
+        const players = await collection.find({}).toArray();
+
+        
+        console.log("Fetch successful!");
+        return players;
+
+    } finally {
+        await client.close();
+        console.log("Disconnected MongoDB!");
     }
-    else if (req.url === '/about'){
-        fs.readFile(path.join(__dirname,'public','about.html'),(err,content)=>{
+}
+const server = http.createServer(async (req, res) => {
+    
+    if (req.url === '/') {
+        // read public.html file from public folder
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(content);
+                        }
+              );
+     }
 
-            if (err) throw err ;
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(content)
-    
-           })
-    
+    else if (req.url==='/api')
+    {
+        try {
+            const players = await fetchPlayers();
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(players, null, 2));
+        } catch (error) {
+            console.error(error);
+            res.writeHead(500, { 'Content-Type': 'text/html' });
+            res.end("Internal Server Error");
+        }
     }
-    else if (req.url ==='/api'){
-        fs.readFile(path.join(__dirname,'public','db.json'),(err,content)=>{
-
-            if (err) throw err ;
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(content)
     
-           })
-    
-    }
     else{
-        res.end("<h1> 404 Nothing is here </h1>")
+        res.end("<h1> 404 nothing is here</h1>");
     }
 
-    
+});
 
-}).listen(1536,()=>console.log("Great our server is running on port 1536"));
+const PORT= process.env.PORT || 1536;
 
- 
+// port, callback
+server.listen(PORT,()=> console.log(`Great our server is running on port ${PORT}`));
